9307da3a73d17379ca4dea14bab3d6ae4bba1ed7
Austin Papritz <austin@papritz.dev>
2022-11-16 14:53:48 -0800

    authenticated sign in correctly redirects - mvp

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import { getUser, redirectIfLoggedIn, signupUser } from './fetch-utils.js';
+import { getUser, redirectIfLoggedIn, signInUser, signupUser } from './fetch-utils.js';
 
 const signInForm = document.getElementById('sign-in');
 
@@ -13,6 +13,16 @@ signUpForm.addEventListener('submit', async (e) => {
     e.preventDefault();
     const data = new FormData(signUpForm);
     const user = await signupUser(data.get('email'), data.get('password'));
+
+    if (user) {
+        location.replace('./other-page');
+    }
+});
+
+signInForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const data = new FormData(signInForm);
+    const user = await signInUser(data.get('email'), data.get('password'));
 
     if (user) {
         location.replace('./other-page');
