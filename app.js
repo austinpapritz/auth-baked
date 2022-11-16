63b104bf0adb3305475049ed67b69e668d27160b
Austin Papritz <austin@papritz.dev>
2022-11-16 12:19:35 -0800

    signUpForm eventListener added but user is logging as undefined

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
+import { getUser, redirectIfLoggedIn, signupUser } from './fetch-utils.js';
+
 const signInForm = document.getElementById('sign-in');
 
 const signUpForm = document.getElementById('sign-up');
 
 // Wire up sign in and sign up forms to supabase
 // Redirect to /other-page on successful auth
+redirectIfLoggedIn();
 // Redirect to /other-page when page loads if user is authenticated
+signUpForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const data = new FormData(signUpForm);
+    const email = data.get('email');
+    const user = await signupUser(email, data.get('password'));
+});
